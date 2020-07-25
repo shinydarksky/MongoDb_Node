2f9ea3710fca54b26a663f04a622503f49ab3858
MK404xL <shinydark.sky@gmail.com>
2020-07-25 21:28:57 +0700

    failed so failed

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
+app.set('views','views');
+app.set('view engine', 'ejs');
 
-
-
-app.get('/',(req,res)=> res.send('hello'))
+app.get('/',(req,res)=>res.render('Home.ejs'))
 
 
 app.listen(3000)
