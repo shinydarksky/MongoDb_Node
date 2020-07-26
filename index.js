995b0794a9d195dae39b92b21c39d22698f86994
MK404xL <shinydark.sky@gmail.com>
2020-07-26 14:32:17 +0700

    Home Screen

--- index.js
+++ index.js
@@ -2,25 +2,26 @@ const express = require('express')
 const app = express()
 app.set('views','views');
 app.set('view engine', 'ejs');
-let mongoose = require('mongoose')
-let option = {
-    db:{native_paser:true},
-    server:{poolSize:5},
-    user:'lelouch',
-    pass:'lelouch'
-}
-mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost:27017/tutorial',option).then(
-    ()=>{
-        console.log("success")
-    },
-    err => {
-        console.log('failed')
-    }
-)
+app.use(express.static('./public'))
+// let mongoose = require('mongoose')
+// let option = {
+//     db:{native_paser:true},
+//     server:{poolSize:5},
+//     user:'lelouch',
+//     pass:'lelouch'
+// }
+// mongoose.Promise = global.Promise
+// mongoose.connect('mongodb://localhost:27017/tutorial',option).then(
+//     ()=>{
+//         console.log("success")
+//     },
+//     err => {
+//         console.log('failed')
+//     }
+// )
 app.get('/',(req,res)=>res.render('Home.ejs'))
-
-
-app.listen(3000)
+app.listen(3000,()=>{
+    console.log(`Start server port ${3000}`)
+})
 
 
